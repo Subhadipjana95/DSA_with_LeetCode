8681bac77b7e347da3632caf01559e2152bedecb
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-01-11 21:14:02 +0530

    Time: 3 ms (74.57%) | Memory: 46.3 MB (79.00%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -22,13 +22,11 @@ class Solution {
 
     private int value(char c){
         if(c == 'I') return 1;
-        else if(c == 'V') return 5;
-        else if(c == 'X') return 10;
-        else if(c == 'L') return 50;
-        else if(c == 'C') return 100;
-        else if(c == 'D') return 500;
-        else{
-            return 1000;
-        }
+        if(c == 'V') return 5;
+        if(c == 'X') return 10;
+        if(c == 'L') return 50;
+        if(c == 'C') return 100;
+        if(c == 'D') return 500;
+        return 1000;
     }
 }
