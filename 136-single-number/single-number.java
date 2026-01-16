2ba8b9db742336009bd9963a549cd6835d74faf2
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-01-16 23:33:27 +0530

    Time: 1 ms (99.85%) | Memory: 46.9 MB (71.92%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 1) return nums[0];
+
+        int result = 0;
+        for(int num: nums){
+            result ^= num;
+        }
+        return result;
+    }
+}
