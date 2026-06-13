b463a9870c7c2761acc9e7d2374e7ffd9512469b
Subhadip Jana <codesubhadip95@gmail.com>
2026-06-13 22:28:36 +0530

    Time: 3 ms (98.83%) | Memory: 44.1 MB (86.37%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,33 +1,32 @@
 class Solution {
     public String reverseWords(String s) {
+        // Trim all the starting & ending trailing spaces
         s = s.trim();
+
         StringBuilder ans = new StringBuilder();
-        int i = s.length() -1;
+        int i = s.length() - 1;
 
         // Traverse from last to first  
-        while(i>=0){
-            // Check for trailing spaces
-            // while(i>= 0 && s.charAt(i) == ' '){
-            //     i--;
-            // }
-            // Now if checking if 'i' has become less than 0
-            if(i<0) break;
+        while (i >= 0) {
+            // Check if 'i' has become less than 0
+            if (i < 0)
+                break;
 
             int j = i;
             // Move 'j' to the start index of the word
-            while(j>=0 && s.charAt(j) != ' '){
+            while (j >= 0 && s.charAt(j) != ' ') {
                 j--;
             }
-            // Append the word in StringBuilder - ans
-            ans.append(s.substring(j+1, i+1));
+            // Append the word in ans
+            ans.append(s.substring(j + 1, i + 1));
 
             // Clear all the trailing ' '(spaces) between two words
-            while(j>=0 && s.charAt(j) == ' '){
+            while (j >= 0 && s.charAt(j) == ' ') {
                 j--;
             }
 
             // Now append ' '(space) only if its not the first word
-            if(j>=0){       // 'j' has been not on the before the first index of first word
+            if (j >= 0) { // Check 'j' has been not on the previous index the first index of first word
                 ans.append(' ');
             }
             // Set the position of 'i' at 'j'th position 
