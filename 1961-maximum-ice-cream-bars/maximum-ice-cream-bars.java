d620ca3f9102d4047be8c15de05d5c2233b35aeb
Subhadip Jana <codesubhadip95@gmail.com>
2026-06-21 21:34:33 +0530

    Time: 12 ms (78.42%) | Memory: 86.6 MB (5.04%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -8,17 +8,31 @@ class Solution {
         }
 
         int count = 0;
-        int spend = 0;
-        for (int i = 0; i < freq.length; i++) {
-            for (int j = freq[i]; j >= 0; j--) {
-                long curr = (long)i * j;
-                if (spend + curr <= coins) {
-                    count += j;
-                    spend += curr;
-                    break;
-                }
-            }
+        // int spend = 0;
+        // for (int i = 0; i < freq.length; i++) {
+        //     for (int j = freq[i]; j >= 0; j--) {
+        //         long curr = (long)i * j;
+        //         if (spend + curr <= coins) {
+        //             count += j;
+        //             spend += curr;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // Better Approach
+        for (int cost = 1; cost <= maxx; cost++) {
+            if (freq[cost] == 0)
+                continue;
+
+            int canBuy = Math.min(freq[cost], coins / cost); // only can buy minimum of available ice creams vs how many can afford 
+            count += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost)
+                continue; // Can't afford any ice creams
         }
+
         return count;
     }
 }
